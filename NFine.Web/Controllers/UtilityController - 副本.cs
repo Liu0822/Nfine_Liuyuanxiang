35304aab1220614e0d06011bdc768c3cbf85fe1a
lyx <67083465@qq.com>
2023-05-26 11:33:58 +0800

    fix: test提交文件

---
+++ NFine.Web/Controllers/UtilityController - 副本.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NFine.Web.Controllers
+{
+    public class UtilityController : Controller
+    {
+    
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ExcelExportForm()
+        {
+            return View();
+        }
+
+    }
+}
